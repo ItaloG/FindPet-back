5c62c6ef1fd9b3f4bbac7f48a051e4b6e5af38c8
italo Gabriel <italog03@gmail.com>
2021-11-30 14:50:30 -0300

    arrumando variaveis de hambiente

--- src/config/firebase-key.js
+++ src/config/firebase-key.js
@@ -2,7 +2,7 @@ module.exports = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: "9d4a1d80cea8d89e9432dc360c9c5fe56358bd7c",
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: "104160850244346597379",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
