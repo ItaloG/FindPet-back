8075962f25478a1b0176b5a22bff1da28b3d5ef5
italo Gabriel <italog03@gmail.com>
2021-12-05 22:20:55 -0300

    rota de cordenadas das instituições

--- src/controllers/cordenadas.js
+++ src/controllers/cordenadas.js
@@ -4,9 +4,19 @@ module.exports = {
   async index(req, res) {
     try {
       const cordenadas = await Institution.findAll({
-        attributes: ["lat", "lng"],
+        attributes: ["nome", "lat", "lng"],
+        include: [
+          {
+            association: "TypeInstitution",
+            attributes: ["type_institution"],
+          },
+          {
+            association: "AddressInstitutions",
+            attributes: ["logradouro", "numero", "complemento"],
+          },
+        ],
       });
-   
+
       return res.status(200).send(cordenadas);
     } catch (error) {
       console.log(error);
